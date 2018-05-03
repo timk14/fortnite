d8a8edd2bee67e4354c6b6c5bb1304869f725ffd
timk14 <tim.konopacki@gmail.com>
2018-05-03 13:10:41 -0500

    first

---
+++ fortnite/menuController.swift
@@ -0,0 +1,35 @@
+//
+//  menuController.swift
+//  fortnite
+//
+//  Created by Timothy P. Konopacki on 5/3/18.
+//  Copyright © 2018 VKT. All rights reserved.
+//
+
+import UIKit
+
+class menuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
